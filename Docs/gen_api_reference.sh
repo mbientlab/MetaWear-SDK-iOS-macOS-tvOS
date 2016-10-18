c30ee7e7ab12ab879b7d40ed3f0be030ec6c5dd9
Stephen Schiffli <stephen@mbientlab.com>
2016-10-18 14:16:35 -0700

    Bumped version number to 2.7.2

--- Docs/gen_api_reference.sh
+++ Docs/gen_api_reference.sh
@@ -1,1 +1,1 @@
-appledoc -p "MetaWear iOS API 2.7.1" --project-version "2.7.1" -c "MBIENTLAB INC" --company-id com.mbientlab --no-create-docset --no-repeat-first-par --ignore .m -o . ../MetaWear/Classes
+appledoc -p "MetaWear iOS API 2.7.2" --project-version "2.7.2" -c "MBIENTLAB INC" --company-id com.mbientlab --no-create-docset --no-repeat-first-par --ignore .m -o . ../MetaWear/Classes
