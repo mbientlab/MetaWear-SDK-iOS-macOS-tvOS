be874ecda35f29d15255f9995e1d1837a3ca1d0e
lkasso <laura@kassovic.com>
2021-01-04 10:48:07 -0800

    release 3.4.3

--- MetaWear/Tests/Integration/Tests.swift
+++ MetaWear/Tests/Integration/Tests.swift
@@ -244,7 +244,7 @@ class Tests: XCTestCase {
             
             mbl_mw_logging_download(board, 0, &self.handlers)
             
-            DispatchQueue.main.asyncAfter(deadline: .now() + 10) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
                 mbl_mw_sensor_fusion_stop(self.device.board)
                 self.counter = 1
                 print("stopping \(Date())")
