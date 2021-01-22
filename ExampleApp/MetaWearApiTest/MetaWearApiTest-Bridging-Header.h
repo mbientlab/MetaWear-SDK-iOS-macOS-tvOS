92b82a76ab00d3330b63b4c327b53a6ffd49b88e
lkasso <laura@kassovic.com>
2021-01-22 14:00:09 -0800

    updated example metawear app released to app store

--- ExampleApp/MetaWearApiTest/MetaWearApiTest-Bridging-Header.h
+++
@@ -1,8 +0,0 @@
-//
-//  MetaWearApiTest-Bridging-Header.h
-//  MetaWearApiTest
-//
-//  Created by Stephen Schiffli on 11/3/16.
-//  Copyright © 2016 MbientLab. All rights reserved.
-//
-#import "APLGraphView.h"
---
+++ ExampleApp/MetaWearApp/MetaWearApp-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  MetaWearApp-Bridging-Header.h
+//  MetaWearApp
+//
+//  Created by Stephen Schiffli on 11/3/16.
+//  Copyright © 2016 MbientLab. All rights reserved.
+//
+#import "APLGraphView.h"
